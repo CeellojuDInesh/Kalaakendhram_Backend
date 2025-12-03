72029f2c39922e4106d6b32788d96df2db024600
Ceelloju Dinesh <2411cs010097@mallareddyuniversity.ac.in>
2025-12-03 20:26:32 +0530

    Configure static files with WhiteNoise and update settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 # -----------------------------
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,6 +123,10 @@ USE_TZ = True
 # -----------------------------
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # -----------------------------
 # DEFAULT FIELD
